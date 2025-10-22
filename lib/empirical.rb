634bf7b671c69a4bebd0437d646db2d25bf78fd5
Stephen Margheim <stephen.margheim@gmail.com>
2025-10-23 00:09:13 +0200

    Move the __process_eval_args__ and __eval_block_from_forwarding__ method definitions into the EvalProcessor file so that they are only defined on Empirical when that file is required.

--- lib/empirical.rb
+++ lib/empirical.rb
@@ -80,49 +80,4 @@ module Empirical
 
 		buffer
 	end
-
-	# For internal use only. This method pre-processes arguments to an eval method.
-	#: (Object, Symbol, *untyped)
-	def self.__process_eval_args__(receiver, method_name, *args)
-		method = METHOD_METHOD.bind_call(receiver, method_name)
-		owner = method.owner
-
-		source, file = nil
-
-		case method_name
-		when :class_eval, :module_eval
-			if Module == owner
-				source, file = args
-			end
-		when :instance_eval
-			if BasicObject == owner
-				source, file = args
-			end
-		when :eval
-			if Kernel == owner
-				source, _binding, file = args
-			elsif Binding == owner
-				source, file = args
-			end
-		end
-
-		if String === source
-			file ||= caller_locations(1, 1).first.path
-
-			if CONFIG.match?(file)
-				args[0] = process(source, with: PROCESSORS)
-			else
-				args[0] = process(source)
-			end
-		end
-
-		args
-	rescue ::NameError
-		args
-	end
-
-	#: () { () -> void } -> Proc
-	def self.__eval_block_from_forwarding__(*, &block)
-		block
-	end
 end
--- lib/empirical/eval_processor.rb
+++ lib/empirical/eval_processor.rb
@@ -1,38 +1,85 @@
 # frozen_string_literal: true
 
-class Empirical::EvalProcessor < Empirical::BaseProcessor
-	EVAL_METHODS = Set[:class_eval, :module_eval, :instance_eval, :eval].freeze
+module Empirical
+	# For internal use only. This method pre-processes arguments to an eval method.
+	#: (Object, Symbol, *untyped)
+	def self.__process_eval_args__(receiver, method_name, *args)
+		method = METHOD_METHOD.bind_call(receiver, method_name)
+		owner = method.owner
 
-	def visit_call_node(node)
-		name = node.name
+		source, file = nil
 
-		if EVAL_METHODS.include?(name) && (arguments = node.arguments)
-			location = arguments.location
-
-			closing = if arguments.contains_forwarding?
-				")), &(::Empirical.__eval_block_from_forwarding__(...))"
-			else
-				"))"
+		case method_name
+		when :class_eval, :module_eval
+			if Module == owner
+				source, file = args
 			end
-
-			if node.receiver
-				receiver_local = "__eval_receiver_#{SecureRandom.hex(8)}__"
-				receiver_location = node.receiver.location
-
-				@annotations.push(
-					[receiver_location.start_character_offset, 0, "(#{receiver_local} = "],
-					[receiver_location.end_character_offset, 0, ")"],
-					[location.start_character_offset, 0, "*(::Empirical.__process_eval_args__(#{receiver_local}, :#{name}, "],
-					[location.end_character_offset, 0, closing]
-				)
-			else
-				@annotations.push(
-					[location.start_character_offset, 0, "*(::Empirical.__process_eval_args__(self, :#{name}, "],
-					[location.end_character_offset, 0, closing]
-				)
+		when :instance_eval
+			if BasicObject == owner
+				source, file = args
+			end
+		when :eval
+			if Kernel == owner
+				source, _binding, file = args
+			elsif Binding == owner
+				source, file = args
 			end
 		end
 
-		super
+		if String === source
+			file ||= caller_locations(1, 1).first.path
+
+			if CONFIG.match?(file)
+				args[0] = process(source, with: PROCESSORS)
+			else
+				args[0] = process(source)
+			end
+		end
+
+		args
+	rescue ::NameError
+		args
+	end
+
+	#: () { () -> void } -> Proc
+	def self.__eval_block_from_forwarding__(*, &block)
+		block
+	end
+
+	class EvalProcessor < Empirical::BaseProcessor
+		EVAL_METHODS = Set[:class_eval, :module_eval, :instance_eval, :eval].freeze
+
+		def visit_call_node(node)
+			name = node.name
+
+			if EVAL_METHODS.include?(name) && (arguments = node.arguments)
+				location = arguments.location
+
+				closing = if arguments.contains_forwarding?
+					")), &(::Empirical.__eval_block_from_forwarding__(...))"
+				else
+					"))"
+				end
+
+				if node.receiver
+					receiver_local = "__eval_receiver_#{SecureRandom.hex(8)}__"
+					receiver_location = node.receiver.location
+
+					@annotations.push(
+						[receiver_location.start_character_offset, 0, "(#{receiver_local} = "],
+						[receiver_location.end_character_offset, 0, ")"],
+						[location.start_character_offset, 0, "*(::Empirical.__process_eval_args__(#{receiver_local}, :#{name}, "],
+						[location.end_character_offset, 0, closing]
+					)
+				else
+					@annotations.push(
+						[location.start_character_offset, 0, "*(::Empirical.__process_eval_args__(self, :#{name}, "],
+						[location.end_character_offset, 0, closing]
+					)
+				end
+			end
+
+			super
+		end
 	end
 end
