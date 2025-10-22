74e3f400929b67142243298deea3173c65a52a5e
Joel Drapper <j.drapper@plane.com>
2025-10-22 02:27:31 +0100

    Fix tests

--- test/signatures.test.rb
+++ test/signatures.test.rb
@@ -94,7 +94,7 @@ test "return type, no args processes" do
 	assert_equal_ruby processed, <<~RUBY
 		def say_hello;__literally_returns__ = (;
 			"Hello World!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -108,7 +108,7 @@ test "_Void return type, no args processes" do
 	assert_equal_ruby processed, <<~RUBY
 		def return_nothing;__literally_returns__ = (;
 			background_work
-		;);binding.assert(__literally_returns__: _Void);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: _Void, method_name: 'return_nothing', context: self) unless _Void === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -122,7 +122,7 @@ test "_Any? return type, no args processes" do
 	assert_equal_ruby processed, <<~RUBY
 		def foo;__literally_returns__ = (;
 			a
-		;);binding.assert(__literally_returns__: _Any?);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: _Any?, method_name: 'foo', context: self) unless _Any? === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -133,10 +133,10 @@ test "return type, positional arg processes" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(name = nil);binding.assert(name: String);__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(name = nil);(raise ::Empirical::TypeError.argument_type_error(name: 'name', value: name, expected: String, method_name: 'say_hello', context: self) unless String === name);__literally_returns__ = (;
 			"Hello #{name}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -147,10 +147,10 @@ test "return type, positional arg with default processes" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(name = ("World"));binding.assert(name: String);__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(name = ("World"));(raise ::Empirical::TypeError.argument_type_error(name: 'name', value: name, expected: String, method_name: 'say_hello', context: self) unless String === name);__literally_returns__ = (;
 			"Hello #{name}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -161,10 +161,10 @@ test "return type, keyword arg processes" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(name: nil);binding.assert(name: String);__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(name: nil);(raise ::Empirical::TypeError.argument_type_error(name: 'name', value: name, expected: String, method_name: 'say_hello', context: self) unless String === name);__literally_returns__ = (;
 			"Hello #{name}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -175,10 +175,10 @@ test "positional and keyword" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(greeting = nil, name: nil);binding.assert(greeting: String, name: String);__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(greeting = nil, name: nil);(raise ::Empirical::TypeError.argument_type_error(name: 'greeting', value: greeting, expected: String, method_name: 'say_hello', context: self) unless String === greeting);(raise ::Empirical::TypeError.argument_type_error(name: 'name', value: name, expected: String, method_name: 'say_hello', context: self) unless String === name);__literally_returns__ = (;
 		  "#{greeting} #{name}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -189,10 +189,10 @@ test "return type, keyword arg with default processes" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(name: ("World"));binding.assert(name: String);__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(name: ("World"));(raise ::Empirical::TypeError.argument_type_error(name: 'name', value: name, expected: String, method_name: 'say_hello', context: self) unless String === name);__literally_returns__ = (;
 			"Hello #{name}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -214,9 +214,9 @@ test "basic" do
 	RUBY
 
 	assert_equal_ruby processed, <<~RUBY
-		def foo(a: nil, b: nil);binding.assert(a: Integer, b: String);__literally_returns__ = (;
+		def foo(a: nil, b: nil);(raise ::Empirical::TypeError.argument_type_error(name: 'a', value: a, expected: Integer, method_name: 'foo', context: self) unless Integer === a);(raise ::Empirical::TypeError.argument_type_error(name: 'b', value: b, expected: String, method_name: 'foo', context: self) unless String === b);__literally_returns__ = (;
 			a
-		;);binding.assert(__literally_returns__: Numeric);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: Numeric, method_name: 'foo', context: self) unless Numeric === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -243,9 +243,9 @@ test "with generic return type" do
 	RUBY
 
 	assert_equal_ruby processed, <<~RUBY
-		def foo(a: nil, b: nil);binding.assert(a: Integer, b: String);__literally_returns__ = (;
+		def foo(a: nil, b: nil);(raise ::Empirical::TypeError.argument_type_error(name: 'a', value: a, expected: Integer, method_name: 'foo', context: self) unless Integer === a);(raise ::Empirical::TypeError.argument_type_error(name: 'b', value: b, expected: String, method_name: 'foo', context: self) unless String === b);__literally_returns__ = (;
 			a
-		;);binding.assert(__literally_returns__: _String(length: 10));__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: _String(length: 10), method_name: 'foo', context: self) unless _String(length: 10) === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -257,9 +257,9 @@ test "with generic input types" do
 	RUBY
 
 	assert_equal_ruby processed, <<~RUBY
-		def foo(a: nil, b: nil);binding.assert(a: _Integer(1..), b: String(length: 10));__literally_returns__ = (;
+		def foo(a: nil, b: nil);(raise ::Empirical::TypeError.argument_type_error(name: 'a', value: a, expected: _Integer(1..), method_name: 'foo', context: self) unless _Integer(1..) === a);(raise ::Empirical::TypeError.argument_type_error(name: 'b', value: b, expected: String(length: 10), method_name: 'foo', context: self) unless String(length: 10) === b);__literally_returns__ = (;
 			a
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'foo', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -271,9 +271,9 @@ test "brace block" do
 	RUBY
 
 	assert_equal_ruby processed, <<~RUBY
-		def foo(a: nil, b: nil);binding.assert(a: Integer, b: String);__literally_returns__ = (;
+		def foo(a: nil, b: nil);(raise ::Empirical::TypeError.argument_type_error(name: 'a', value: a, expected: Integer, method_name: 'foo', context: self) unless Integer === a);(raise ::Empirical::TypeError.argument_type_error(name: 'b', value: b, expected: String, method_name: 'foo', context: self) unless String === b);__literally_returns__ = (;
 			a
-		;);binding.assert(__literally_returns__: Numeric);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: Numeric, method_name: 'foo', context: self) unless Numeric === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -295,10 +295,10 @@ test "return type, keyword arg with default processes" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(*names );binding.assert(names: ::Literal::_Array(String));__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(*names);(raise ::Empirical::TypeError.argument_type_error(name: 'names', value: names, expected: ::Literal::_Array(String), method_name: 'say_hello', context: self) unless ::Literal::_Array(String) === names);__literally_returns__ = (;
 			"Hello #{names.join(", ")}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -309,10 +309,10 @@ test "return type, keyword arg with default processes" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(*names );binding.assert(names: ::Literal::_Array(_Deferred { foo }));__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(*names);(raise ::Empirical::TypeError.argument_type_error(name: 'names', value: names, expected: ::Literal::_Array(_Deferred { foo }), method_name: 'say_hello', context: self) unless ::Literal::_Array(_Deferred { foo }) === names);__literally_returns__ = (;
 			"Hello #{names.join(", ")}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -323,10 +323,10 @@ test "return type, keyword arg with default processes" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(names = nil);binding.assert(names: ([String]));__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(names = nil);(raise ::Empirical::TypeError.argument_type_error(name: 'names', value: names, expected: ([String]), method_name: 'say_hello', context: self) unless ([String]) === names);__literally_returns__ = (;
 			"Hello #{names.join(", ")}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -337,10 +337,10 @@ test "return type, keyword arg with default processes" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(**names);binding.assert(names: ::Literal::_Hash(_Deferred { foo }, String));__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(**names);;__literally_returns__ = (;
 			"Hello #{names.join(", ")}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -351,10 +351,10 @@ test "return type, keyword arg with default processes" do
 		end
 	RUBY
 
-	assert_equal_ruby(processed, <<~'RUBY')
-		def say_hello(names: nil);binding.assert(names: ({_Deferred { foo } => String}));__literally_returns__ = (;
+	assert_equal(processed, <<~'RUBY')
+		def say_hello(names: nil);(raise ::Empirical::TypeError.argument_type_error(name: 'names', value: names, expected: ({_Deferred { foo } => String}), method_name: 'say_hello', context: self) unless ({_Deferred { foo } => String}) === names);__literally_returns__ = (;
 			"Hello #{names.join(", ")}!"
-		;);binding.assert(__literally_returns__: String);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: String, method_name: 'say_hello', context: self) unless String === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -365,10 +365,10 @@ test "arg splat with named type" do
 		end
 	RUBY
 
-	assert_equal processed, <<~RUBY
-		def move_to(*position );binding.assert(position: Position);__literally_returns__ = (;
+	assert_equal_ruby processed, <<~RUBY
+		def move_to(*position);(raise ::Empirical::TypeError.argument_type_error(name: 'position', value: position, expected: Position, method_name: 'move_to', context: self) unless Position === position);__literally_returns__ = (;
 			do_something
-		;);binding.assert(__literally_returns__: _Void);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: _Void, method_name: 'move_to', context: self) unless _Void === __literally_returns__);__literally_returns__;end
 	RUBY
 end
 
@@ -379,9 +379,9 @@ test "kwarg splat with named type" do
 		end
 	RUBY
 
-	assert_equal processed, <<~RUBY
-		def move_to(**position);binding.assert(position: Position);__literally_returns__ = (;
+	assert_equal_ruby processed, <<~RUBY
+		def move_to(**position);(raise ::Empirical::TypeError.argument_type_error(name: 'position', value: position, expected: Position, method_name: 'move_to', context: self) unless Position === position);__literally_returns__ = (;
 			do_something
-		;);binding.assert(__literally_returns__: _Void);__literally_returns__;end
+		;);(raise ::Empirical::TypeError.return_type_error(value: __literally_returns__, expected: _Void, method_name: 'move_to', context: self) unless _Void === __literally_returns__);__literally_returns__;end
 	RUBY
 end
