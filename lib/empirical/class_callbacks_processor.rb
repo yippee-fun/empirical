82f7aae243eb49ac7094e79087947456260c0ba2
Joel Drapper <j.drapper@plane.com>
2025-10-23 18:36:41 +0100

    Disable class/module callbacks for now
    There’s some problem with them.

--- lib/empirical/class_callbacks_processor.rb
+++ lib/empirical/class_callbacks_processor.rb
@@ -13,11 +13,11 @@ class Class
 end
 
 class Empirical::ClassCallbacksProcessor < Empirical::BaseProcessor
-	def visit_class_node(node)
-		@annotations << [node.end_keyword_loc.start_offset, 0, ";class_defined();"]
-	end
+	# def visit_class_node(node)
+	# 	@annotations << [node.end_keyword_loc.start_offset, 0, ";class_defined();"]
+	# end
 
-	def visit_module_node(node)
-		@annotations << [node.end_keyword_loc.start_offset, 0, ";module_defined();"]
-	end
+	# def visit_module_node(node)
+	# 	@annotations << [node.end_keyword_loc.start_offset, 0, ";module_defined();"]
+	# end
 end
