8749d4ad7064e087b8c7757de987d394fc5ab697
Joel Drapper <j.drapper@plane.com>
2025-10-24 13:11:38 +0100

    Update rubocop rule to allow for forwarding defs

--- lib/rubocop/cop/empirical/no_defs.rb
+++ lib/rubocop/cop/empirical/no_defs.rb
@@ -4,10 +4,10 @@ class RuboCop::Cop::Empirical::NoDefs < RuboCop::Cop::Base
 	MSG = "Use `fun` method definitions instead of `def` method definitions."
 
 	def on_def(node)
-		add_offense(node)
+		add_offense(node) unless node.arguments.any?(&:forward_args_type?)
 	end
 
 	def on_defs(node)
-		add_offense(node)
+		add_offense(node) unless node.arguments.any?(&:forward_args_type?)
 	end
 end
