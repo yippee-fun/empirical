8e15a8b49c46d43f81f1df483b7f64070058ce7c
Joel Drapper <j.drapper@plane.com>
2025-10-29 22:59:10 +0000

    Support typed properties and instance variable write guards

---
+++ test/prop_signatures.test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class User
+	@name :: String
+	@name / String
+	@name ~= String
+	@name = String
+	@name | String
+	@name |= String
+	@name => String
+
+	let @name => String
+	let @name => String
+
+	prop :name, String
+
+
+	attr_reader :name
+
+	def good
+		@name = "Hello"
+	end
+
+	def bad
+		@name = 1
+	end
+end
+
+test do
+	user = User.new
+
+	user.good
+
+	assert_equal "Hello", user.name
+
+	assert_raises TypeError do
+		user.bad
+	end
+end
